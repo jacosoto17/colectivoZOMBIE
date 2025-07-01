7b3fbf803cf8a570411696f62affc8b3e2169f2b
Jacobo Soto  Rojas <jacobo.soto@globant.com>
2025-07-01 06:56:26 -0500

    feat se cran variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner keyEntry = new Scanner(System.in);
+        System.out.println("***** RESIDENT EVIL CODE BY GLOBERS *****");
 
+        //PERSONAJE Leon S Kennedy
+        //nombre
+        //edad
+        //cantidad de vida
+
+        //personaje MR X (TITAN)
+        //nombre
+        //virus que lo infecta
+        //cantidadVida
+
+        final String LeonSKennedy, MRX, virus;
+        final byte edad;
+        final int cantidadVida;
 
 
     }
